6190cf9217a1aa773a57c650b1521cf0b5a83ca3
Jessica Cayaban <jest.cayaban@gmail.com>
2026-01-18 16:05:22 +0900

    Finished chapter 5

--- chap05/dns_query.cpp
+++ chap05/dns_query.cpp
@@ -42,13 +42,14 @@ void print_dns_message(const char* message, int msg_length) {
   const unsigned char* msg = (const unsigned char*) message;
   // This prints out the whole text, so we should only print it out
   // if we want to see the whole message
+  /*
   int i;
   for (i = 0; i < msg_length; ++i) {
     unsigned char r = msg[i];
     printf("%02d:   %02x %03d '%c'\n", i, r, r, r);
   }
   printf("\n");
-
+  */
   printf("ID = %0X %0X\n", msg[0], msg[1]);
 
   const int qr = (msg[2] & 0x80) >> 7;
@@ -203,7 +204,120 @@ void print_dns_message(const char* message, int msg_length) {
   }
 
   printf("\n");
-
-
-
 } // print_dns_message
+
+int main(int argc, char* argv[]) {
+  if (argc < 3) {
+    printf("Usage:\n\tdns_query hostname type\n");
+    printf("Example:\n\tdns_query example.com aaaa\n");
+    exit(0);
+  }
+
+  if (strlen(argv[1]) > 255) {
+    fprintf(stderr, "Hostname too long.");
+    exit(1);
+  }
+
+  unsigned char type;
+  if (strcmp(argv[2], "a") == 0) {
+    type = 1;
+  } else if (strcmp(argv[2], "mx") == 0) {
+    type = 15;
+  } else if (strcmp(argv[2], "txt") == 0) {
+    type = 16;
+  } else if (strcmp(argv[2], "aaaa") == 0) {
+    type = 28;
+  } else if (strcmp(argv[2], "any") == 0) {
+    type = 255;
+  } else {
+    fprintf(stderr, "Unknown type '%s'. Use a, aaaa, txt, mx, or any.",
+            argv[2]);
+    exit(1);
+  }
+
+#if defined(_WIN32)
+  WSADATA d;
+  if (WSAStartup(MAKEWORD(2, 2), &d)) {
+    fprintf(stderr, "Failed to initialize.\n");
+    return 1;
+  }
+#endif
+
+  printf("Configuring remote address...\n");
+  struct addrinfo hints;
+  memset(&hints, 0, sizeof(hints));
+  hints.ai_socktype = SOCK_DGRAM;
+  struct addrinfo* peer_address;
+  if (getaddrinfo("8.8.8.8", "53", &hints, &peer_address)) {
+    fprintf(stderr, "getaddrinfo() failed. (%d)\n", GETSOCKETERRNO());
+    return 1;
+  }
+
+  printf("Creating socket...\n");
+  SOCKET socket_peer;
+  socket_peer = socket(peer_address->ai_family,
+                       peer_address->ai_socktype,
+                       peer_address->ai_protocol);
+  if(!ISVALIDSOCKET(socket_peer)) {
+    fprintf(stderr, "socket() failed. (%d)\n", GETSOCKETERRNO());
+    return 1;
+  }
+
+  char query[1024] = {static_cast<char>(0xAB),
+                      static_cast<char>(0xCD), // ID
+                      0x01, 0x00, // set recursion
+                      0x00, 0x01, // QDCOUNT
+                      0x00, 0x00, // ANCOUNT
+                      0x00, 0x00, // NSCOUNT
+                      0x00, 0x00 // ARCOUNT
+  };
+
+  char* p = query + 12;
+  char* h = argv[1];
+
+  while(*h) {
+    char* len = p;
+    p++;
+    if (h != argv[1]) ++h;
+
+    while (*h && *h != '.') *p++ = *h++;
+    *len = p - len - 1;
+  }
+
+  *p++ = 0;
+
+  *p++ = 0x00;
+  *p++ = type; // QTYPE
+
+  *p++ = 0x00;
+  *p++ = 0x01; //QCLASS
+
+  const int query_size = p - query;
+
+  int bytes_sent = sendto(socket_peer,
+                          query, query_size,
+                          0,
+                          peer_address->ai_addr,
+                          peer_address->ai_addrlen);
+  printf("Sent %d bytes.\n", bytes_sent);
+
+  // Check if query was correct or if we typoed
+  print_dns_message(query, query_size);
+
+  char read[1024];
+  int bytes_received = recvfrom(socket_peer, read,
+                                1024, 0, 0, 0);
+  printf("Received %d bytes.\n", bytes_received);
+
+  print_dns_message(read, bytes_received);
+  printf("\n");
+
+  freeaddrinfo(peer_address);
+  CLOSESOCKET(socket_peer);
+
+#if defined(_WIN32)
+  WSACleanup();
+#endif
+
+  return 0;
+}// main
