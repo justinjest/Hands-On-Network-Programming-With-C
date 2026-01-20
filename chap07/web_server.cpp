c3b506e484fb609ce34746764269e4aa49ee6a08
Jessica Cayaban <jest.cayaban@gmail.com>
2026-01-21 07:47:20 +0900

    Finished simple server from chap07

--- chap07/web_server.cpp
+++ chap07/web_server.cpp
@@ -96,3 +96,222 @@ struct client_info *get_client(SOCKET s) {
   clients = n;
   return n;
 }
+
+
+void drop_client(struct client_info* client) {
+  CLOSESOCKET(client->socket);
+
+  struct client_info **p = &clients;
+
+  while (*p) {
+    if (*p == client) {
+      *p = client->next;
+      free(client);
+      return;
+    }
+    p = &(*p)->next;
+  }
+
+  fprintf(stderr, "drop_client not found.\n");
+  exit(1);
+}
+
+const char* get_client_address(struct client_info* ci) {
+  static char address_buffer[100];
+  getnameinfo((struct sockaddr*) &ci->address,
+              ci->address_length,
+              address_buffer,
+              sizeof(address_buffer), 0, 0,
+              NI_NUMERICHOST);
+  return address_buffer;
+}
+
+fd_set wait_on_clients(SOCKET server) {
+  fd_set reads;
+  FD_ZERO(&reads);
+  FD_SET(server, &reads);
+  SOCKET max_socket = server;
+  struct client_info* ci = clients;
+
+  while(ci) {
+    FD_SET(ci->socket, &reads);
+    if (ci->socket > max_socket) {
+      max_socket = ci->socket;
+    }
+    ci = ci->next;
+  }
+
+  if (select(max_socket+1, &reads, 0, 0, 0) < 0) {
+    fprintf(stderr, "select() failed. (%d)\n", GETSOCKETERRNO());
+    exit(1);
+  }
+
+  return reads;
+}
+
+void send_400(struct client_info* client) {
+  const char* c400 = "HTTP/1.1 400 Bad Request\r\n"
+    "Connection: close\r\n"
+    "Content-Length: 11\r\n\r\nBad Request";
+  send(client->socket, c400, strlen(c400), 0);
+  drop_client(client);
+}
+
+
+void send_404(struct client_info* client) {
+  const char* c404 = "HTTP/1.1 404 Not Found\r\n"
+    "Connection: close\r\n"
+    "Content-Length: 11\r\n\r\nNot Found";
+  send(client->socket, c404, strlen(c404), 0);
+  drop_client(client);
+}
+
+void serve_resource(struct client_info* client, const char* path) {
+  printf("serve_resource %s %s\n", get_client_address(client), path);
+
+  if (strcmp(path, "/") == 0) path = "/index.html"; // Sets default to index
+
+  if (strlen(path) > 100) {
+    send_400(client);
+    return;
+  }
+
+  if (strstr(path, "..")) {
+    send_404(client);
+    return;
+  }
+
+  char full_path[128];
+  sprintf(full_path, "public%s", path);
+
+#if defined(_WIN32)
+  char *p = full_path;
+  while (*p) {
+    if (*p == '/') *p = '\\';
+    ++p;
+  }
+#endif
+
+  FILE *fp = fopen(full_path, "rb");
+
+  if (!fp) {
+    send_404(client);
+    return;
+  }
+
+  fseek(fp, 0L, SEEK_END);
+  size_t cl = ftell(fp);
+  rewind(fp);
+
+  const char* ct = get_content_type(full_path);
+
+#define BSIZE 1024
+  char buffer[BSIZE];
+
+  sprintf(buffer, "HTTP/1.1 200 OK\r\n");
+  send(client->socket, buffer, strlen(buffer), 0);
+
+
+  sprintf(buffer, "Connection: close\r\n");
+  send(client->socket, buffer, strlen(buffer), 0);
+
+  sprintf(buffer, "Content-Length: %u\r\n", static_cast<int>(cl));
+  send(client->socket, buffer, strlen(buffer), 0);
+
+
+  sprintf(buffer, "Content-Type: %s\r\n", ct);
+  send(client->socket, buffer, strlen(buffer), 0);
+
+  sprintf(buffer, "\r\n");
+  send(client->socket, buffer, strlen(buffer), 0);
+
+  int r = fread(buffer, 1, BSIZE, fp);
+  while (r) {
+    send(client->socket, buffer, r, 0);
+    r = fread(buffer, 1, BSIZE, fp);
+  }
+
+  fclose(fp);
+  drop_client(client);
+}
+
+int main() {
+#if defined(_WIN32)
+  if (WSAStartup(MAKEWORD(2, 2), &d)) {
+    fprintf(stderr, "Failed to initalize.\n");
+    return 1;
+  }
+#endif
+  // if we chance 127.0.0.1 to 0 we will compile this w/ outside access
+  SOCKET server = create_socket("127.0.0.1", "8080");
+
+  while(1) {
+
+    fd_set reads;
+    reads = wait_on_clients(server);
+
+    if (FD_ISSET(server, &reads)) {
+      struct client_info* client = get_client(-1);
+
+      client->socket = accept(server,
+                              (struct sockaddr*) &(client->address),
+                              &(client->address_length));
+      if (!ISVALIDSOCKET(client->socket)) {
+        fprintf(stderr, "accept() failed. (%d)\n",
+                GETSOCKETERRNO());
+        return 1;
+      }
+
+      printf("New connection from %s.\n",
+             get_client_address(client));
+    }
+    struct client_info* client = clients;
+    while(client) {
+      struct client_info* next = client->next;
+
+      if (FD_ISSET(client->socket, &reads)) {
+        if (MAX_REQUEST_SIZE == client->received) {
+          send_400(client);
+          continue;
+        }
+        int r = recv(client->socket,
+                     client->request + client->received,
+                     MAX_REQUEST_SIZE - client->received, 0);
+        if (r < 1) {
+          printf("Unexpected disconnect from %s.\n",
+                 get_client_address(client));
+          drop_client(client);
+        } else {
+          client->received += r;
+          client->request[client->received] = 0;
+
+          char* q = strstr(client->request, "\r\n\r\n");
+          if (q) {
+            if (strncmp("GET /", client->request, 5)) {
+              send_400(client);
+            } else {
+              char* path = client->request + 4;
+              char* end_path = strstr(path, " ");
+              if (!end_path) {
+                send_400(client);
+              } else {
+                *end_path = 0;
+                serve_resource(client, path);
+              }
+            }
+          } // if (q)
+        }
+      }
+      client = next;
+    }
+  } // while(1)
+  printf("\nCLosing socket...\n");
+  CLOSESOCKET(server);
+
+#if defined(_WIN32)
+  WSACleanup();
+#endif
+
+  printf("Finished.\n");
+  return 0;
+}
