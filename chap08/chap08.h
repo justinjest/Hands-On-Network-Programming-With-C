0136a824153ecb08aedbc48a104eaf37c91029cd
Jessica Cayaban <jest.cayaban@gmail.com>
2026-01-22 07:41:04 +0900

    Finished chap 08 basic smtp server

---
+++ chap08/chap08.h
@@ -0,0 +1,40 @@
+#ifndef CHAP08_H_
+#define CHAP08_H_
+
+#if defined(_WIN32)
+#ifndef _WIN32_WINNT
+#define _WIN32_WINNT 0x0600
+#endif // _WIN32_WINNT
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#pragma comment (lib, "ws2_32.lib")
+
+#else
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <unistd.h>
+#include <errno.h>
+
+#endif // if defined win32
+
+#if defined(_WIN32)
+#define ISVALIDSOCKET(s) ((s) != INVALID_SOCKET)
+#define CLOSESOCKET(s) closesocket(s)
+#define GETSOCKETERRNO() (WSAGetLastError())
+
+#else
+#define ISVALIDSOCKET(s) ((s) >= 0)
+#define CLOSESOCKET(s) close(s)
+#define SOCKET int
+#define GETSOCKETERRNO() (errno)
+#endif // if defined _win32
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+#endif // CHAP08_H_
